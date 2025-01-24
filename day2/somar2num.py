30dbcf0ee7fd5e45beb82760ad6cf87d8d5e1392
RichardJr <richardjunior_o_cara_@hotmail.com>
2025-01-24 11:55:02 -0300

    segundo dia do projeto

---
+++ day2/somar2num.py
@@ -0,0 +1,6 @@
+numero1 = input("Digite um numero:")
+numero2 = input("Digite outro numero:") 
+
+soma = int(numero1) + int(numero2)
+
+print(f"Essa é a soma de dois numeros em python : {soma}")
---
+++ day2/somar2numV2.py
@@ -0,0 +1,13 @@
+def somadoisnum(num1,num2):
+    soma = num1+num2
+    return soma
+
+#print(somadoisnum(3,7))
+
+def soma2numeros():
+    n1=int(input("Coloque o primeiro numero:"))
+    n2=int(input("Coloque o segundo numero:"))
+    soma2num = n1 + n2
+    return soma2num
+
+print(soma2numeros())
