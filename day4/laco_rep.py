cdd5120a7ec008adb71b2498fdfe857810b432f4
RichardJr <richardjunior_o_cara_@hotmail.com>
2025-01-24 14:25:53 -0300

    add dia 4

---
+++ day4/laco_rep.py
@@ -0,0 +1,11 @@
+#usando o for para imprimir numeros de 0 a 10
+for numero in range(11):
+    print(numero)
+    
+print("\n")
+
+# usando while para imprimir numeros de 0 a 10
+num = 0
+while num < 11:
+    print(num)
+    num += 1
