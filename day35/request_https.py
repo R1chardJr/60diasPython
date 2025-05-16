e786e5260132e7a69afd0a2582b89377ace71d7d
RichardJr <richardjunior_o_cara_@hotmail.com>
2025-05-16 13:00:36 -0300

    fix:ajuste dia 35

--- day35/request_https.py
+++ day35/request_https.py
@@ -7,8 +7,6 @@ def obter_joke(url):
 
     if resposta.status_code != 200:
         print('Erro ao acessar a API')
-        print(f'Código de status: {resposta.status_code}')
-        print(f'Mensagem de erro: {resposta.text}')
         return None
         
     data = resposta.json()
@@ -19,7 +17,7 @@ def obter_joke(url):
     resultado = tradutor.translate(data['value'], src='en', dest='pt')
     
     print("PT\nEssa é a piada do Chuck Norris:")
-    print(resultado.text)  # Hello, world!
+    print(resultado.text) 
 
 if __name__ == '__main__':
     url = 'https://api.chucknorris.io/jokes/random'
